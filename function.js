d559f38067e809bf13c32c373cecb48d6d6bc5d9
najmus sakib <najmuskg@gmail.com>
2024-01-25 15:13:21 +0600

    select option markup added

--- function.js
+++ function.js
@@ -1,43 +1,54 @@
-// file2.js
-// file2.js
-const importedVariable = require('./file1.js');
-console.log(importedVariable); // Output: Hello, World!
+// $('#person-select').change(function(){
+//   //this is just getting the value that is selected
+//   var title = $(this).val();
+//   $('.modal-title').html(title);
+//   $('.modal').modal('show');
+// });
 
-
-import { sakib } from './familiyMemberData.js';
-
-document.querySelectorAll(".filter-title").forEach(function (element) {
-  element.addEventListener("click", function () {
-    this.dataset.clicked = true;
-    var clickedElement = this;
-
-    document.querySelectorAll(".filter-title").forEach(function (item) {
-      if (item !== clickedElement) {
-        item.nextElementSibling.classList.remove("filter-active");
-      }
-    });
-
-    this.nextElementSibling.classList.toggle("filter-active");
-  });
-});
-
-document.querySelectorAll(".cloak").forEach(function (element) {
-  element.addEventListener("click", function () {
-    this.parentElement.classList.remove("filter-active");
-  });
-});
-
-// import the variables and function from module.js
-
-
-
-
+const personsData = [
+  {
+    id: "sajjad",
+    name: "Sajedul Karim Bhuiyan",
+    age: 42,
+    occupation: "Backend Developer",
+  },
+  {
+    id: "akib",
+    name: "Shuaib Hasan Akib",
+    age: 28,
+    occupation: "Software Engineer",
+  },
+  {
+    id: "sakib",
+    name: "Najmus Sakib",
+    age: 24,
+    occupation: "Developer",
+  },
+  {
+    id: "afzal",
+    name: "FM Afjalul Bari",
+    age: 42,
+    occupation: "Student",
+  },
+  {
+    id: "jakir",
+    name: "Jakir Hosen",
+    age: 24,
+    occupation: "Student",
+  },
+  {
+    id: "rafat",
+    name: "Farhanur Rahman Rafat",
+    age: 42,
+    occupation: "Student",
+  },
+];
 
 const person = document.querySelectorAll(".person");
 
 person.forEach((element) => {
-    element.addEventListener("click", function () {
-    const selectedValue = this.getAttribute("data-value");
+  element.addEventListener("click", function () {
+    const selectedValue = this.getAttribute("value");
     personDataShow(selectedValue);
   });
 });
@@ -49,7 +60,6 @@ function personDataShow(selectedValue) {
   );
 
   if (selectedPerson) {
-
     const infoText = `Name: ${selectedPerson.name}.<br>Age: ${selectedPerson.age}.<br>Occupation: ${selectedPerson.occupation}`;
     dataShow.innerHTML = infoText;
   } else {
